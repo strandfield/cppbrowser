5b33b25f6cb67aed11e39bed3e9c5d140b848f6d
strandfield <vincent.chambrin@gmail.com>
2024-08-07 10:28:45 +0200

    highlight matching parts

--- client/src/components/SnapshotFileSearchResultItem.vue
+++ client/src/components/SnapshotFileSearchResultItem.vue
@@ -1,6 +1,6 @@
 <script setup>
 
-import { ref, computed, inject } from 'vue'
+import { computed, inject } from 'vue'
 
 const props = defineProps({
   matchResult: {
@@ -12,6 +12,35 @@ const props = defineProps({
 const projectName = inject('projectName');
 const projectRevision = inject('projectRevision');
 
+const matchParts = computed(() => {
+  let result = [];
+  let text = props.matchResult.element;
+  let i = 0;
+  for (const letter_index of props.matchResult.match) {
+    if (i < letter_index) {
+      result.push({
+        class: "non-matching-text",
+        text: text.substring(i, letter_index)
+      });
+    }
+
+    result.push({
+        class: "matching-text",
+        text: text[letter_index]
+      });
+    i = letter_index + 1;
+  }
+
+  if (i < text.length) {
+    result.push({
+      class: "non-matching-text",
+      text: text.substring(i)
+    });
+  }
+
+  return result;
+});
+
 function splitPath(path) {
   return path.split("/");
 }
@@ -22,10 +51,12 @@ function splitPath(path) {
   <li>
     <RouterLink
       :to="{ name: 'file', params: { projectName: projectName, projectRevision: projectRevision, pathParts: splitPath(matchResult.element) } }">
-      {{ matchResult.element }}</RouterLink> ({{ matchResult.score }})
+      <span v-for="(part,index) in matchParts" :key="index" :class="part.class">{{ part.text }}</span></RouterLink> ({{ matchResult.score }})
   </li>
 </template>
 
 <style scoped>
-
+.matching-text {
+  font-weight: bold;
+}
 </style>
