52f4b6ec1a4cfc8880a53268e2615587f7b33113
strandfield <vincent.chambrin@gmail.com>
2024-08-20 20:16:16 +0200

    fix 404 error when serving vue.js frontend with express
    This is a quickfix. We will need to handle routing and 404 errors
    more seriously in future versions.

--- server/app.js
+++ server/app.js
@@ -128,20 +128,27 @@ app.use(app.locals.site.baseUrl, express.static(path.join(__dirname, 'public')))
 //app.use(app.locals.site.baseUrl, express.static(path.join(__dirname, 'dist')));
 app.use(app.locals.site.baseUrl, indexRouter);
 
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
-});
+if (fs.existsSync(path.join(__dirname, 'dist') + "/index.html")) {
+  console.log("serving vue.js SPA");
+  app.use(function (req, res, next) {
+    res.sendFile(path.join(__dirname, 'dist') + "/index.html");
+  });
+} else {
+  // catch 404 and forward to error handler
+  app.use(function (req, res, next) {
+    next(createError(404));
+  });
 
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+  // error handler
+  app.use(function (err, req, res, next) {
+    // set locals, only providing error in development
+    res.locals.message = err.message;
+    res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
-});
+    // render the error page
+    res.status(err.status || 500);
+    res.render('error');
+  });
+}
 
 module.exports = app;
