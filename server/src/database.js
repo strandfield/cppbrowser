4854b4524839772dc112ff77d5c9d75d198f01a8
strandfield <vincent.chambrin@gmail.com>
2024-12-30 09:17:18 +0100

    update databases to latest schema

--- server/src/database.js
+++ server/src/database.js
@@ -1,13 +1,19 @@
 
 
 
-const Database = require('better-sqlite3');
+// const Database = require('better-sqlite3');
 
 function checkTableExists(db, tableName) {
     let stmt = db.prepare("SELECT name FROM sqlite_master WHERE type='table' AND name = ?");
     return stmt.get(tableName) != undefined;
 }
 
+function checkColumnExists(databaseObject, tableName, columnName) {
+    let stmt = databaseObject.prepare(`SELECT * FROM pragma_table_info('${tableName}') WHERE name=?`);
+    return stmt.get(columnName) != undefined;
+}
+
 module.exports = { 
-    checkTableExists
+    checkTableExists,
+    checkColumnExists
 };
